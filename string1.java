d6309d7e417b7ae249d40384e3d89dcb31179411
Shubham.Prasad() <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-07-05 15:53:02 +0530

    Update string1.java

--- string1.java
+++ string1.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
-public class string1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a String: ");
-        String input = sc.nextLine();
+public class charchange {
+    public static String charF(String input){
         int length = input.length();
+        String output = "";
+        char c;
         for (int i = 0; i < length; i++){
-            char c = input.charAt(i);
+            c = input.charAt(i);
             if (c >= 65 && c <= 90){
                 c += 32;
-                System.out.print(c);
+                output+=c;
             }
             else if (c >= 97 && c <= 122){
                 c -= 32;
-                System.out.print(c);
+                output+=c;
             }
             else
-                System.out.print(c);
+                output+=c;
         }
+        return output;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String s = sc.nextLine();
+        System.out.println(charF(s));
     }
 }
