4a3dfcf905c6175c40d6c1ef683b380565e103f5
Shubham.Prasad() <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-07-05 16:46:44 +0530

    Create string2.java

---
+++ string2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class strRev {
+    public static String rev(String input){
+        String output = "";
+        for (int l = (input.length() - 1); l >= 0; l--){
+            output += input.charAt(l);
+        }
+        return output;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String in = sc.nextLine();
+        System.out.println(rev(in));
+    }
+}
