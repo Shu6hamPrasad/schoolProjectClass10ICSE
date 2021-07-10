6d8f12d9c0329dcfd72240379d6aafe4fccbd56d
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-07-10 09:27:08 +0530

    Update and rename marks.java to conditional2.java

--- conditional2.java
+++ conditional2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class marks {
+public class conditional2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your marks: ");
